951783a63be8edd7736e276bbcb1e068c91b1879
Uchimata <cheap.sheep.u@gmail.com>
2009-05-17 00:11:56 +0900

    -a オプションにプレイリストURLを指定できるように修正
    ・YouTube::VideoURI -> Video::YouTube[::URI]
    ・プレイリスト一括ダウンロード、腐った正規表現と何も考えてないURL修正でだいぶ適当に実装した

--- yt.pl
+++ yt.pl
@@ -20,7 +20,7 @@ use Getopt::Long;
 local $| = 1;
 
 our $PROGRAM_NAME = 'YoutubeDownloader';
-our $VERSION      = '0.3.0';
+our $VERSION      = '0.4.0';
 
 my (
 	@link, $list, $dir, $all, $test,
@@ -48,7 +48,7 @@ print <<"_HELP_" and exit if $help;
 $PROGRAM_NAME - Ver. $VERSION
 $0 [-a | --list <downloadlist_filename>] [-d | --directory <downloaddir>] [-l | --href <URL>]
   -l or --href: input URLs. (ex. -l URL -l URL -l URL ...)
-  -a or --list: list of target. input filename. default is 'yt.txt'.
+  -a or --list: list of target. input a filename or a playlist URL. default is 'yt.txt'.
   -d or --download: download directory. input dirname. default is current.
   -v: show download progress indication.
   -f: set &fmt. default is undefind. if you set this option without arguments, use -f=18.
@@ -56,6 +56,7 @@ $0 [-a | --list <downloadlist_filename>] [-d | --directory <downloaddir>] [-l | 
             7=[176x144 ? 3gp],     13=[176x144 MPEG4 3gp],
            18=[480x270 H.264 mp4], 22=[1280x720 H.264 mp4],
            34=[320x180 H.264 flv], 35=[640x380 H.264 flv]  )
+  -s: download while disregarding any notices.
   -b: beep when some errors have happened.
   -r: avoid extreme fragmenting of videos.
   -t: test mode.
@@ -70,7 +71,7 @@ main();
 
 
 sub main {
-	my $yt = YouTube::VideoURI->new(dir => $dir, all => $all, progress => $verbose, fmt => $fmt);
+#	my $yt = Video::YouTube->new();
 	my @lines = map { split /\s+/ } @link;
 
 	my $TEMP_DIR = File::Spec->tmpdir;
@@ -88,8 +89,13 @@ sub main {
 	}
 
 	unless (@lines) {
-		my $fh = file($list)->open('<:utf8') or die "cannot open '$list'\n";
-		@lines = $fh->getlines;
+		if ($list =~ m!^https?://!) {
+			@lines = Video::YouTube->new->get_video_uri($list)->uris;
+		}
+		else {
+			my $fh = file($enc->encode($list))->open('<:utf8') or die "cannot open '$list'\n";
+			@lines = $fh->getlines;
+		}
 	}
 
 	print "test mode start. \n\n" if $test;
@@ -99,13 +105,12 @@ sub main {
 		if ($test) {
 			print "get_video_uri: ",(/[?&]v=([^&=]+)/)[0],"... ";
 
-			$yt->get_video_uri($_);
+			my $yt = Video::YouTube::URI->new($_, fmt => $fmt);
 
 			print "ok.\n", "\tdownload uri: " . $yt->uri . "\n\n";
 		}
 		else {
-			download($yt, $_);
-
+			download(Video::YouTube::URI->new($_, fmt => $fmt));
 		}
 	}
 
@@ -115,25 +120,21 @@ sub main {
 
 sub _path {
 	my ($s, $a, $t) = @_;
-	my $dir = $a ? $temp : $s->dir;
-	my $tmp = (!$a and $t) ? "-$s->{title_tmp}" : "";
+	my $DIR = $a ? $temp : $dir;
+	my $TMP = (!$a and $t) ? "-$s->{title_tmp}" : "";
 
-	file( $enc->encode($dir), $enc->encode(join('', $s->title, ".", $s->ext, $tmp)) );
+	file( $enc->encode($DIR), $enc->encode(join('', $s->title, ".", $s->ext, $TMP)) );
 }
 
 sub download {
-	my ($self, $uri) = @_;
+	my ($self, %opt) = @_;
 
 	my $download;
-
-	if ($uri) {
-		$self->get_video_uri($uri);
-	}
 
 	print $self->title, '.', $self->ext, "\n";
 
 	if (-e _path($self)) {
-		if (!$self->{all}) {
+		if (!$all) {
 			my $anser = '';
 			while ($anser !~ /^y(?:es)?$|^no?$/) {
 				print "\talready exists. download? (yes/no), $beep";
@@ -148,8 +149,8 @@ sub download {
 
 	print "\tdownload ... ";
 
-	if (!$self->{progress}) {
-		my $res = $self->{ua}->mirror( $self->{uri}, _path($self, $avoid) );
+	if (!$verbose) {
+		my $res = $self->{ua}->mirror( $self->uri, _path($self, $avoid) );
 		print $res->is_success ? "ok." : $res->status_line, "\n";
 		if ($res->is_success and $avoid) {
 			print "\tcopy from TEMP ... ";
@@ -176,7 +177,7 @@ sub download {
 		$wfh->binmode();
 
 		my $total = 0;
-		my $res = $self->{ua}->get($self->{uri}, ':content_cb' => sub {
+		my $res = $self->{ua}->get($self->uri, ':content_cb' => sub {
 			my ( $chunk, $res, $proto ) = @_;
 			print $wfh $chunk;
 			my $size = $wfh->tell;
@@ -209,43 +210,74 @@ sub download {
 }
 
 
-package YouTube::VideoURI;
+package Video::YouTube;
 
 use utf8;
 use strict;
 use warnings;
 
-binmode STDIN  => ":encoding(cp932)";
-binmode STDOUT => ":encoding(cp932)";
-binmode STDERR => ":encoding(cp932)";
-
-use Carp;
-use URI;
 use Encode qw(decode_utf8);
 use LWP::UserAgent;
 
-
-our $VERSION = '0.2.0';
+our $VERSION = '0.3.0';
 
 sub new {
 	my $class  = shift;
 	my %opt    = @_;
 
 	return bless({
-		dir => $opt{dir},
-		ua  => LWP::UserAgent->new(),
-		all => $opt{all},
-		progress => $opt{progress},
-		fmt => $opt{fmt},
-		ext => ($opt{fmt} ==  7 or $opt{fmt} == 13) ? '3gp'
-		    :  ($opt{fmt} == 18 or $opt{fmt} == 22) ? 'mp4'
-		                                            : 'flv',
+		ua  => ($opt{ua} or LWP::UserAgent->new),
+		def_fmt => $opt{fmt},
 	}, $class);
 }
 
 sub get_video_uri {
-	my ($self, $uri) = @_;
+	my ($self, $uri, %opt) = @_;
+	$opt{ua} ||= $self->{ua};
 
+	my $res = $self->{ua}->get($uri);
+	croak($res->status_line) if $res->is_error;
+
+	my $content = decode_utf8($res->content);
+	my %u; $u{''}++;
+	while (!$u{
+		my $next = ($content =~ m!<a[^>]+?href="([^"]+)"[^>]+?class="pagerNotCurrent"[^>]+>!ig)[-1] || ''
+	}++) {
+		push(@{$self->{uris}}, ($content =~ /<a id="video-long-title-[^"]+"\s*href="([^"]+)"/ig));
+		my $res = $self->{ua}->get($next);
+		croak($res->status_line) if $res->is_error;
+		$content = decode_utf8($res->content);
+	}
+
+#	push(@{$self->{uris}}, Video::YouTube::URI->new($uri, %opt));
+
+	$self;
+}
+
+sub uris {
+	my $uris = shift->{uris};
+	return wantarray ? @{$uris} : $uris;
+}
+
+
+package Video::YouTube::URI;
+
+use utf8;
+use strict;
+use warnings;
+
+use Carp;
+use URI;
+use Encode qw(decode_utf8);
+use LWP::UserAgent;
+
+sub new {
+	my $self = bless { fmt => 0 }, shift;
+	my $uri = shift;
+	my %opt = @_;
+	$self->{ua} = $opt{ua} || LWP::UserAgent->new;
+
+	$uri = "http://www.youtube.com/$uri" if $uri !~ /^http/;
 	my $uri_c = URI->new($uri);
 	my %params = $uri_c->query_form;
 	$params{v} ||= '';
@@ -258,21 +290,18 @@ sub get_video_uri {
 
 	if ($content =~ /video_id=([^&]*).*?&t=([^&]*)(?:.*?&title=(.*)';)?/) {
 		$self->{uri}  = "http://www.youtube.com/get_video?video_id=$1&t=$2";
-		$self->{uri} .= "&fmt=$self->{fmt}" if $self->{fmt};
 		$self->{title} = $3 || '_';
 		$self->{title} =~ s!&amp;!&!g;
 		$self->{title} =~ s![\\/:*?"<>|]!_!g;
 	}
 
-	$self->{uri};
+	$self->fmt($opt{fmt});
+
+	$self;
 }
 
 sub uri {
 	return shift->{uri} || undef;
-}
-
-sub dir {
-	return shift->{dir} || undef;
 }
 
 sub title {
@@ -280,7 +309,27 @@ sub title {
 }
 
 sub ext {
-	return shift->{ext} || undef;
+	my $self = shift;
+	return ($self->fmt ==  7 or $self->fmt == 13) ? '3gp'
+	     : ($self->fmt == 18 or $self->fmt == 22) ? 'mp4'
+	                                              : 'flv';
+}
+
+sub fmt {
+	my ($self, $fmt) = @_;
+	if ($fmt && $self->is_defined($fmt)) {
+		$self->{uri} .= "&fmt=$fmt";
+		$self->{fmt} = $fmt;
+	}
+
+	$self->{fmt} = 0 unless exists $self->{fmt};
+	return $self->{fmt};
+}
+
+sub is_defined {
+	my ($self, $fmt) = @_;
+	my $uri = $self->{uri} . ($fmt ? "&fmt=$fmt" : '');
+	return $self->{ua}->head($uri)->is_success;
 }
 
 
